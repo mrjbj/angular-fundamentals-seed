d46a8c26bfac39abf3245ca97ea1c715d44db6a0
Jason Jones <jason@brucejones.biz>
2018-05-24 11:20:52 -0400

    events to edit and remove

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -7,7 +7,16 @@ import { Passenger } from '../../models/passenger.interface';
     template: `
     <div>
         <span class="status" [class.checked-in] = "detail.checkedIn" ></span>
-        {{ detail.fullname }}
+        <div *ngIf="editing">
+            <input 
+                type="text" 
+                [value]="detail.fullname"
+                (input) = 'onNameChange(name.value)'
+                #name >
+        </div> 
+        <div *ngIf="!editing">
+            {{ detail.fullname }}
+        </div>
         <div class="date" >
             Check in date:
             {{ detail.checkInDate ? (detail.checkInDate | date: 'yMMMd') : 0 }}
@@ -15,13 +24,46 @@ import { Passenger } from '../../models/passenger.interface';
         <div class="children">
             Children: {{detail.children?.length || 0 }}
         </div>
+        <button (click)="toggleEdit()">
+            {{editing ? 'Done' : 'Edit'}}
+        </button> 
+        <button (click)="onRemove()">
+           Remove
+        </button>
     </div>
   `  
 })
 
 export class PassengerDetailComponent {
+    // PROPERTIES
+    editing: boolean = false;
+    
     @Input()  // tag 'detail' property as bindable from selector (e.g. <passenger-detail>)
     detail: Passenger;
+    
+    // EVENTS
+    // tag 'remove' as property emitted up to selector (e.g. <passenger-detail>
+    // class that has <passenger-detail> in its template will need to handle
+    // the event via function defined int he parent class locally)
+    @Output()
+    remove: EventEmitter<any> = new EventEmitter();   
+    @Output()
+    edit: EventEmitter<any> = new EventEmitter();    
+
+    // FUNCTIONS
     constructor() { }
+
+    onNameChange(value: string) {
+        this.detail.fullname = value; 
+    }
+    toggleEdit() {
+        if (this.editing) {
+            this.edit.emit(this.detail);
+        }
+        this.editing = !this.editing;
+    }
+    onRemove() {
+        this.remove.emit(this.detail);
+    }
 
 }
--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -11,6 +11,8 @@ import { Passenger} from '../../models/passenger.interface'
     // @Component is decorating that class, so 'passengers' property is in local scope. 
     // [items] and [detail] is binding to custom property, so need to decorate that 
     // attribute in the child class with @Input to flag it as a bindabnle from parents.
+    // 'remove' is property defined in passenger-detail that emits events. 
+    // (remove) means to listen for remove events and handle them here in passenger-dashboard. 
     template: `
      <div>
         <passenger-count
@@ -18,7 +20,9 @@ import { Passenger} from '../../models/passenger.interface'
         </passenger-count>
          <passenger-detail
             *ngFor="let passenger of passengers;"
-            [detail]="passenger">
+            [detail]="passenger"
+            (edit)="handleEdit($event)"
+            (remove)="handleRemove($event)">
         </passenger-detail>       
     </div>
     `
@@ -61,5 +65,20 @@ export class PassengerDashboardComponent implements OnInit{
             checkInDate: null,
             children: null
         }];
+    }
+    handleEdit(event: Passenger) {
+        this.passengers = this.passengers.map((passenger: Passenger) => {
+            if (passenger.id === event.id) {
+                // we are on the passenger that event says might have been edited
+                passenger = Object.assign({}, passenger, event);
+            }
+            return passenger;
+        })
+        console.log(this.passengers);
+       
+    }
+    handleRemove(event: Passenger) {
+        this.passengers = this.passengers.filter(
+            (passenger: Passenger) => passenger.id !== event.id);
     }
 }
